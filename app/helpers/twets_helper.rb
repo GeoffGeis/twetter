e9019ebb13f54114e0b1fae5d4a9b09dbb7d8d58
GeoffGeis <geoffgeis@hotmail.com>
2014-04-15 14:41:56 -0500

    first foundation for gravatar helper method

--- app/helpers/twets_helper.rb
+++ app/helpers/twets_helper.rb
@@ -1,5 +1,13 @@
+require 'digest/md5'
+
 module TwetsHelper
   def point_profiles(twet)
     twet.gsub(/(?<!\w)@(\w+)/) { |match| "<a href=\"#{match[1..-1]}\">#{match}</a>" }.html_safe
+  end
+
+  def gravatar
+    email_address = @user.email.downcase
+    hash = Digest::MD5.hexdigest(email_address)
+    image_src = "http://www.gravatar.com/avatar/#{hash}"
   end
 end
